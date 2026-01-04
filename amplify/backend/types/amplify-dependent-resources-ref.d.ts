ae84fbbec63d2a346327b8fed7165ddf9be54281
mike evans <mike.evans@flbreaker.com>
2026-01-04 15:43:33 -0500

    implimented amplify hosting.  now hosted on aws

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cardmaker2b70d674": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "users": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
